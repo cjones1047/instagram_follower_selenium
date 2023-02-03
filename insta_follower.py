70e73fc7d5d326cabe2e1f5ecd09cef16811842d
cjones1047 <cjmj1047@gmail.com>
2023-02-03 16:56:33 -0600

    find_followers method uses exception handling to ensure search field is only interacted with when the entire Instagram home page loads after login

--- insta_follower.py
+++ insta_follower.py
@@ -24,51 +24,58 @@ class InstaFollower:
     def login(self):
         ig_login_url = "https://www.instagram.com/"
         self.driver.get(ig_login_url)
-        time.sleep(1)
+        time.sleep(5)
         username_input_el = self.driver.find_element(by=By.NAME, value="username")
         username_input_el.click()
         username_input_el.send_keys(self.ig_username, Keys.TAB, self.ig_password, Keys.ENTER)
-        time.sleep(3)
-        try:
-            avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_acan _acao '
-                                                                                    '_acas _aj1-"]')
-            avoid_save_info_el.click()
-            time.sleep(1)
-        except NoSuchElementException:
-            pass
-        try:
-            avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='span[class="_2iem"]')
-            avoid_save_info_el.click()
-            time.sleep(1)
-        except NoSuchElementException:
-            pass
-        # log_in_button = self.driver.find_element(by=By.CSS_SELECTOR, value='a[href="/accounts/login/?next=%2F&'
-        #                                                                    'source=logged_out_half_sheet"]')
-        # log_in_button.click()
+        # time.sleep(3)
+        # try:
+        #     avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_acan _acao '
+        #                                                                             '_acas _aj1-"]')
+        #     avoid_save_info_el.click()
+        #     time.sleep(1)
+        # except NoSuchElementException:
+        #     pass
+        # try:
+        #     avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='span[class="_2iem"]')
+        #     avoid_save_info_el.click()
+        #     time.sleep(1)
+        # except NoSuchElementException:
+        #     pass
+        # # log_in_button = self.driver.find_element(by=By.CSS_SELECTOR, value='a[href="/accounts/login/?next=%2F&'
+        # #                                                                    'source=logged_out_half_sheet"]')
+        # # log_in_button.click()
+        # # time.sleep(1)
+        # # username_input_el = self.driver.find_element(by=By.NAME, value="username")
+        # # username_input_el.click()
+        # # username_input_el.send_keys(self.ig_username, Keys.TAB, self.ig_password, Keys.ENTER)
+        # # time.sleep(1)
+        # # avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_acan _acao '
+        # #                                                                         '_acas _aj1-"]')
+        # # avoid_save_info_el.click()
+        # # time.sleep(1)
+        # avoid_notifications_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_a9-- _a9_1"]')
+        # avoid_notifications_el.click()
         # time.sleep(1)
-        # username_input_el = self.driver.find_element(by=By.NAME, value="username")
-        # username_input_el.click()
-        # username_input_el.send_keys(self.ig_username, Keys.TAB, self.ig_password, Keys.ENTER)
-        # time.sleep(1)
-        # avoid_save_info_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_acan _acao '
-        #                                                                         '_acas _aj1-"]')
-        # avoid_save_info_el.click()
-        # time.sleep(1)
-        avoid_notifications_el = self.driver.find_element(by=By.CSS_SELECTOR, value='button[class="_a9-- _a9_1"]')
-        avoid_notifications_el.click()
-        time.sleep(1)
 
     def find_followers(self):
-        search_button_el = self.driver.find_element(by=By.CSS_SELECTOR, value='svg[aria-label="Search"]')
-        search_button_el.click()
-        time.sleep(1)
-        search_field_el = self.driver.find_element(by=By.CSS_SELECTOR, value='input[aria-label="Search input"]')
-        search_field_el.click()
-        search_field_el.send_keys(SEARCH_WORD)
-        time.sleep(1)
-        account_name_els_list = self.driver.find_elements(by=By.CSS_SELECTOR, value='div[class="_aacl _aaco _aacw '
-                                                                                    '_aacx _aad6"]')
-        self.account_name_list = [account.text for account in account_name_els_list]
+        followers_found = False
+        while not followers_found:
+            try:
+                search_button_el = self.driver.find_element(by=By.CSS_SELECTOR, value='svg[aria-label="Search"]')
+            except NoSuchElementException:
+                pass
+            else:
+                search_button_el.click()
+                time.sleep(1)
+                search_field_el = self.driver.find_element(by=By.CSS_SELECTOR, value='input[aria-label="Search input"]')
+                search_field_el.click()
+                search_field_el.send_keys(SEARCH_WORD)
+                time.sleep(1)
+                account_name_els_list = self.driver.find_elements(by=By.CSS_SELECTOR, value='div[class="_aacl _aaco _aacw '
+                                                                                            '_aacx _aad6"]')
+                self.account_name_list = [account.text for account in account_name_els_list]
+                followers_found = True
 
     def follow(self):
         print(self.account_name_list)
